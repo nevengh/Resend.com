e11fb4842e0c2664257d200053f46adfc330afb2
Neven <nevenghasoun@gmail.com>
2024-01-23 23:31:59 -1100

    Resend

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,24 @@
 import './App.css'
 import Container from './Components/Container/Container'
-import Companies from './Sections/Companies/Companies'
-import DevelopEmail from './Sections/DevelopEmail/DevelopEmail'
-import EmailReimagened from './Sections/EmailReimagened/EmailReimagened'
-import EverythingInControl from './Sections/EverythingInControl/EverythingInControl'
-import FirstClass from './Sections/FirstClass/FirstClass'
-
-import Footer from './Sections/Footer/Footer'
-
-
-import Hero from './Sections/Hero/Hero'
-import Integrate from './Sections/Integrate/Integrate'
-
-import NavBar from './Sections/NavBar/NavBar'
-
-import ReachHuman from './Sections/ReachHuman/ReachHuman'
-import SubSectionOne from './Sections/SubSectionOne/SubSectionOne'
-import SubSectionTwo from './Sections/SubSectionTwo/SubSectionTwo'
+import {Hero , Footer,NavBar,Companies, ReachHuman,EverythingInControl,SubSectionOne,SubSectionTwo,FirstClass,EmailReimagened,DevelopEmail,Integrate} from './Sections/index'
 
 
 const App = () => {
   return (
     <>
-      {/* <NavBar/>
       
-      <Hero/>
-      <Companies/>
-      <Integrate/> */}
       <NavBar/>
       <Container>
-        
-      <Hero/>
-      <Companies/>
-      <Integrate/>
-      <FirstClass/>
-      {/* <FirstClassDeveloper/> */}
-      <DevelopEmail/>
-      <ReachHuman/>
-      <SubSectionOne/>
-      <EverythingInControl/>
-      
-      
-      
-      <SubSectionTwo/>
-      <EmailReimagened/>
-      {/* <Companies/> */}
+        <Hero/>
+        <Companies/>
+        <Integrate/>
+        <FirstClass/>
+        <DevelopEmail/>
+        <ReachHuman/>
+        <SubSectionOne/>
+        <EverythingInControl/>
+        <SubSectionTwo/>
+        <EmailReimagened/>
       </Container>
       <Footer/>
     </>
